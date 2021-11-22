8ff781fc21f5832b864d4c999d7786d1867ef5cb
Alex Cooksey <aecooksey2651@gmail.com>
2021-11-22 14:56:32 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random() * 100);
+
+let num3 = 30
+let num4 = 13
+const mod = num3 % num4;
+
+const max = Math.max(4, 17, 20, 14, 9)
